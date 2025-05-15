bb3973f7db4aaac83cb558ce9e7197ad59b71f0e
Niharika0104 <goulikarniharika@gmail.com>
2025-05-15 18:19:26 +0530

    two  list items in one row

--- src/components/custom-components/ListView.tsx
+++ src/components/custom-components/ListView.tsx
@@ -11,34 +11,27 @@ interface ListViewComponentProps {
 
 function ListView({ items }: ListViewComponentProps) {
   return (
-    <div className="space-y-4">
+     <div className="grid grid-cols-2 gap-4">
       {items.map((item, index) => (
         <div key={index} className="p-4 border rounded-lg shadow">
-       <div className='flex flex-col gap-2'>
- <ImageCarousel images={item.Images} />
-        <div>
-          <h3 className="font-bold">{item.Name}</h3>
-          <div className='flex gap-3'>
-           <MapPin className='text-red-400' />
-            <span>{item.Region}</span>
-
+          <div className='flex flex-col gap-2'>
+            <ImageCarousel images={item.Images} />
+            <div>
+              <h3 className="font-bold text-xl">{item.Name}</h3>
+              <div className='flex gap-3 items-center mt-2'>
+                <MapPin className='text-red-400 h-5 w-5' />
+                <span className="text-base">{item.Region}</span>
+              </div>
+              <div className='flex gap-3 items-center mt-2'>
+                <LandPlot className='text-gray-400 h-5 w-5' />
+                <span className="text-base">{item.size}</span>
+              </div>
+              <div className="mt-3">
+                <p className='font-semibold text-lg mb-2'>Owners:</p>
+                <AvatarGroup users={item.Owners}/>
+              </div>
+            </div>
           </div>
-            <div className='flex gap-3'>
-           <LandPlot className='text-gray-400' />
-            <span>{item.size}</span>
-
-          </div>
-
-         
-          
-          <div>
-            <p className='font-semibold text-lg'>Owners:</p>
-         <AvatarGroup users={item.Owners}/>
-          </div>
-        </div>
-       
-          
-        </div>
         </div>
       ))}
     </div>
